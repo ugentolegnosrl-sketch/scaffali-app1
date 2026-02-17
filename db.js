155985f9b35795f67264342e864e143d696943b1
ugentolegnosrl-sketch <ugentolegnosrl@gmail.com>
2026-02-17 14:05:38 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 import postgres from 'postgres'
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL,
 const sql = postgres(connectionString)
+
 
 export default sql
